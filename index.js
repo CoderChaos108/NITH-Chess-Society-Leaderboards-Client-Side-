bed356d99ffc42c101896aff78ff877b96fc6233
CoderChaos108 <dhiraj@venturepoint.net>
2026-02-05 23:54:22 +0530

    added links to usernames

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 const users = [
     {rank:"rank",id:"username",rating:"rating",batch:"batch"},
-    {rank:1,id:"aryan01",rating:2410,batch:2026},
-    {rank:2,id:"chaos02",rating:2335,batch:2027},
+    {rank:1,id:"aryan",rating:2410,batch:2026},
+    {rank:2,id:"chaos108",rating:2335,batch:2027},
     {rank:3,id:"drunkenMaster04",rating:2290,batch:2026},
-    {rank:4,id:"lakshaya04",rating:2215,batch:2028},
+    {rank:4,id:"lakshaysteta",rating:2215,batch:2028},
     {rank:5,id:"nishant05",rating:2150,batch:2029},
     {rank:6,id:"vivek06",rating:2095,batch:2027},
     {rank:7,id:"rahul07",rating:2050,batch:2028},
@@ -17,7 +17,7 @@ const users = [
     {rank:11,id:"siddharth11",rating:1845,batch:2028},
     {rank:12,id:"manan12",rating:1800,batch:2026},
     {rank:13,id:"harsh13",rating:1760,batch:2029},
-    {rank:14,id:"dev14",rating:1725,batch:2027},
+    {rank:14,id:"dev14",rating:1725,batch:2027},    
     {rank:15,id:"arjun15",rating:1690,batch:2028},
     {rank:16,id:"om16",rating:1655,batch:2026},
     {rank:17,id:"tanish17",rating:1620,batch:2029},
